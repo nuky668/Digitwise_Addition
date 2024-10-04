9083dd77de7058fdf4e1cd7fcef8c9c6f5e10cef
David Bárta <nuken668@gmail.com>
2024-10-04 17:40:28 +0200

    Den 2 - program zjednodušen, přeměněn na funkci, stále nefunguje pro vysoká čísla - řešení viz chatGPT

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 ### Digitwise Addition ###
 
-a = 0
-n = 6789
-k = 4
+ # Test Constraints:
+    # 1 <= n <= 10 ** 9
+    # 1 <= k <= 10 ** 5
+    # Expected Time Complexity: O(k * log(n))
 
-# hlavní cyklus 
-while a != k:
-    # 1. čísla zvětšíme o 1 a naházíme do listu
-    n = str(n)
-    list1 = []
-    for digit in n:
-        digit = int(digit) + 1
-        list1.append(digit)
+def digitwise_addition(n, k):
+   """K krát přidá 1 ke každému číslu v N"""
 
-    del n
-    n = ""
+   # hlavní cyklus
+   while k != 0:
+        # 1. čísla zvětšíme o 1 a naházíme do listu
+        n = str(n)
+        list1 = []
+        for digit in n:
+            digit = int(digit) + 1
+            list1.append(digit)
 
-    # 2. vybereme čísla z listu a uděláme proměnnou n
-    for item in list1:
-        n = n + str(item)
-    n = int(n)
+        n = ""
 
-    print(n)        # kontrolní výpis
-    a += 1
+        # 2. vybereme čísla z listu a uděláme proměnnou n
+        for item in list1:
+            n = n + str(item)
+
+        k -= 1
+
+   return len(str(n))   # kolik číslic má upravené číslo "n"
+    
 
 
-
+a = digitwise_addition(789, 3)
+print(a)
